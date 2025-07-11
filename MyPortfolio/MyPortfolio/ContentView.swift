b658531e7d4eb06b49f1803b619eef75297d394a
Smit275-z <w0849357@myscc.ca>
2025-07-10 22:33:05 -0300

    Text Added

--- MyPortfolio/MyPortfolio/ContentView.swift
+++ MyPortfolio/MyPortfolio/ContentView.swift
@@ -4,14 +4,28 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 16) {
             Image("myPhoto")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 150, height: 150)
-                            .clipShape(Circle())
-                            .overlay(
-                                Circle().stroke(Color.blue, lineWidth: 4)
-                            )
-                            .shadow(radius: 7)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.blue, lineWidth: 4)
+                )
+                .shadow(radius: 7)
+            
+            Text("Smit Patel")
+                .font(.title)
+                .bold()
+            Text("Diploma Computer Programming")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            VStack {
+               Image(systemName: "graduationcap.fill")
+               Text("Saint clair college")
+                   Text("2025 graduation year")
+           }
+           .font(.subheadline)
         }
         .padding()
     }
